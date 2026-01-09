f0920b4f0615fb9c3dd09d9aa803f69f202feac3
karthik <karthikmotupalli34@gmail.com>
2026-01-09 20:40:00 +0530

    changed

--- databasemodels.py
+++ databasemodels.py
@@ -2,8 +2,10 @@ from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 Base=declarative_base()
 class Users(Base):
+    age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal,target,cp_type,sex,target_str
     __tablename__="database"
     id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
     age = Column(Integer, nullable=False)
     sex = Column(Integer, nullable=False)
     cp = Column(Integer, nullable=False)
--- heart/data.py
+++ heart/data.py
@@ -1,16 +1,17 @@
 from pydantic import BaseModel
+
 class UserCreate(BaseModel):
- 
-   age: int
-   sex: int
-   cp: int
-   trestbps: int
-   chol: float        
-   fbs: int
-   restecg: int
-   thalach: float
-   exang: int
-   oldpeak: float
-   slope: int
-   ca: int
-   thal: int
+    age: int
+    sex: int
+    cp: int
+    trestbps: int
+    chol: float
+    fbs: int
+    restecg: int
+    thalach: float
+    exang: int
+    oldpeak: float
+    slope: int
+    ca: int
+    thal: int
+    cp_type: str
